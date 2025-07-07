25ea01fbd15b43d5fb5dab4f88c5df437e4fbdd1
zhaoyuwang <zhaoyuwang@global.tencent.com>
2025-07-07 23:18:52 +0800

    redis

---
+++ infra/redis.go
@@ -0,0 +1,37 @@
+package infra
+
+import (
+	"context"
+	"fmt"
+	"time"
+	"sync"
+	"github.com/redis/go-redis/v9"
+)
+
+var (
+	redisClient *redis.Client
+	once sync.Once
+)
+func RDB() *redis.Client {
+	once.Do(func() {
+		addr := getenv("REDIS_ADDR", "redis:6379")
+		rdb = redis.NewClient(&redis.Options{Addr:addr})
+	})
+	return rdb
+}
+const lockLua = `if redis.call('SETNX',KEYS[1],ARGV[1]) == 1 
+	then redis.call('PEXPIRE',KEYS[1],ARGV[2] ); return 1 else return 0 END`
+
+const unlockLua = `if redis.call('GET',KEYS[1]) == ARGV[1] then 
+	return redis.call('DEL',KEYS[1]); else return 0 end`
+
+func Acquire(ctx context.Context, key string, ttl time.Duration) (token string, ok bool, err error) {
+	token = uuid.New().String()
+	ok, err = RDB().Eval(ctx, lockLua, []string{key}, token, ttl.Milliseconds()).Bool()
+	return
+}
+
+func Release(ctx context.Context, key string, token string) (ok bool, err error) {
+	ok, err = RDB().Eval(ctx, unlockLua, []string{key}, token).Bool()
+	return
+}
